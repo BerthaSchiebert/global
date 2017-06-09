7736f047965679f3bf13cad139f9f9fbb5c84504
BerthaSchiebert <shirinova.valeria@yandex.ru>
2017-06-09 15:37:39 +0300

    global verstka

--- js/script.js
+++ js/script.js
@@ -5,4 +5,28 @@ $(document).ready(function(){
 		navText: "",
 		loop: true
 	});
+
+	$('.popup-img').magnificPopup({
+	  type: 'image'
+	  // other options
+	});
+
+	$(".sec-6-item").click(function(){
+		$(this).find(".sec-6-text").slideToggle(300);
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
 });
+
+function toggleCP() {
+	var cp = document.getElementById("cp");
+	cp.style.height = window.innerHeight - 0+"px";
+	if(cp.style.left == "0px"){
+		cp.style.left = "-320px";
+	} else {
+		cp.style.left = "0px";
+	}
+}
